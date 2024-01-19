79638b356a78a92e892d4fe72b0d44e5db4d1b67
gonHid <gon.hidalgo.ch@gmail.com>
2024-01-19 13:03:46 -0300

    Ventas funcionales, si el usuario añade 2 veces el mismo producto las cantidades se suman en codigo, se restan los stocks cada vez que se añade un producto a la lista, el boton cancelar devuelve los stocks a su estado anterior.

--- app/src/main/java/bd/stock/njoystick/Venta.java
+++ app/src/main/java/bd/stock/njoystick/Venta.java
@@ -1,11 +1,11 @@
 package bd.stock.njoystick;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
@@ -17,12 +17,16 @@ import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import bd.stock.njoystick.databinding.VentaBinding; // Asegúrate de importar la clase correcta
 
 public class Venta extends AppCompatActivity {
 
     VentaBinding binding; // Utiliza la clase de binding correcta
     private ArrayAdapter<String> listaProductosAdapter;
+    private ArrayList<ProductoVenta> ControlStock = new ArrayList<>();
     private Producto productoStored = null;
     private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(), result -> {
         if (result.getContents() == null) {
@@ -47,19 +51,10 @@ public class Venta extends AppCompatActivity {
         listaProductosAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         binding.listaProductos.setAdapter(listaProductosAdapter);
 
-        binding.btnLeerCodigo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                escanear();
-            }
-        });
-
-        binding.btnAddALista.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                enviarALista();
-            }
-        });
+        binding.btnLeerCodigo.setOnClickListener(view -> escanear());
+        binding.btnAddALista.setOnClickListener(view -> enviarALista());
+        binding.btnRealizarVenta.setOnClickListener(view -> procesarCompra());
+        binding.btnCancelarVenta.setOnClickListener(view -> cancelarCompra());
     }
 
     public void escanear() {
@@ -79,7 +74,7 @@ public class Venta extends AppCompatActivity {
         DatabaseReference productosRef = FirebaseDatabase.getInstance().getReference("productos").child(codigoProducto);
         productosRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
                     Producto producto = dataSnapshot.getValue(Producto.class);
                     if (producto != null) {
@@ -96,28 +91,97 @@ public class Venta extends AppCompatActivity {
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
                 Toast.makeText(getApplicationContext(), "Error al leer datos de Firebase", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    public void procesarCompra(){
+        ArrayAdapter<String> adapter = (ArrayAdapter<String>) binding.listaProductos.getAdapter();
+        adapter.clear();
+        adapter.notifyDataSetChanged();
+        limpiarCampos();
+        ControlStock.clear();
+    }
+
+    public void cancelarCompra() {
+        try {
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("productos");
+
+            for (ProductoVenta productoVenta : ControlStock) {
+                Producto producto = productoVenta.getProducto();
+                int cantidadVenta = productoVenta.getCantidad();
+
+                // Calcula la nueva cantidad después de cancelar la venta
+                int nuevaCantidad = producto.getCantidad() + cantidadVenta;
+
+                // Añade la actualización al lote
+                databaseReference.child(producto.getCodigo()).child("cantidad").setValue(nuevaCantidad);
+            }
+
+            // Limpiar la lista de productos vendidos
+            ArrayAdapter<String> adapter = (ArrayAdapter<String>) binding.listaProductos.getAdapter();
+            adapter.clear();
+            adapter.notifyDataSetChanged();
+            ControlStock.clear();
+
+            // Limpiar la interfaz de usuario
+            limpiarCampos();
+
+            Toast.makeText(getApplicationContext(), "Venta cancelada", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error al cancelar la venta: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
     private void enviarALista(){
         if(productoStored!=null){
-            int cantidad = Integer.parseInt(binding.editTextCantidad.getText().toString());
-            // Añade el nombre del producto a la lista
-            listaProductosAdapter.add(productoStored.getNombre() + " " + productoStored.getMarca() + " cant: "+cantidad);
+            int cantidadVenta = Integer.parseInt(binding.editTextCantidad.getText().toString());
 
-            // Notifica al adaptador que los datos han cambiado
-            listaProductosAdapter.notifyDataSetChanged();
-            binding.imageViewProducto.setImageResource(R.drawable.placeholder_image);
-            binding.textViewNombreProducto.setText("");
-            binding.editTextCantidad.setText("");
-            binding.textViewPrecio.setText("");
-            productoStored=null;
+            // Asegurar de que haya suficiente stock disponible
+            if (productoStored.getCantidad() >= cantidadVenta) {
+                // actualización bdd
+                DatabaseReference productoRef = FirebaseDatabase.getInstance().getReference("productos").child(productoStored.getCodigo());
+                int nuevaCantidad = productoStored.getCantidad() - cantidadVenta;
+                productoRef.child("cantidad").setValue(nuevaCantidad);
+                productoStored.setCantidad(nuevaCantidad);
+                ProductoVenta prodAux = new ProductoVenta();
+                prodAux.cantidad = cantidadVenta;
+                boolean productoExistente = false;
+
+                for (ProductoVenta productoVenta : ControlStock) {
+                    if (productoVenta.getProducto().getCodigo().equals(productoStored.getCodigo())) {
+                        // Producto ya existe en la lista, actualiza la cantidad
+                        productoVenta.setCantidad(productoVenta.getCantidad() + cantidadVenta);
+                        productoVenta.producto = productoStored;
+                        productoExistente = true;
+                        break;
+                    }
+                }
+                if(!productoExistente){
+
+                    prodAux.producto = productoStored;
+                    ControlStock.add(prodAux);
+                }
+               //mover a lista UI
+                listaProductosAdapter.add(productoStored.getNombre() + " " + productoStored.getMarca() + " cant: " + prodAux.cantidad);
+                listaProductosAdapter.notifyDataSetChanged();
+
+                limpiarCampos();
+                productoStored = null;
+            } else {
+                Toast.makeText(getApplicationContext(), "Stock insuficiente", Toast.LENGTH_SHORT).show();
+            }
         }else{
             Toast.makeText(getApplicationContext(), "Debe buscar un nuevo producto", Toast.LENGTH_SHORT).show();
         }
+    }
 
+    private void limpiarCampos(){
+        //Limpiar campos
+        binding.imageViewProducto.setImageResource(R.drawable.placeholder_image);
+        binding.textViewNombreProducto.setText("");
+        binding.editTextCantidad.setText("");
+        binding.textViewPrecio.setText("");
     }
 }
